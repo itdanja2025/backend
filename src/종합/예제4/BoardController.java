aba53de8fbebb13911d4b04e1a54de6ed434fd9e
itdanja2025 <itdanja2025@gmail.com>
2026-01-27 15:10:44 +0900

    day06

---
+++ src/종합/예제4/BoardController.java
@@ -0,0 +1,23 @@
+package 종합.예제4;
+// - 게시물기능 설계클래스
+public class BoardController {
+    Board[ ] boards = new Board[ 100 ];     // 속성 : 멤버변수
+    // 기능1 : 저장 메소드
+    boolean doPost( String content , String writer ){
+        Board board = new Board(); // 1. 객체 생성
+        board.content = content; // 2. 매개변수로 받은 값을 객체에 대입
+        board.writer = writer;
+        for( int index = 0 ; index <= boards.length - 1 ; index++ ){
+            if( boards[index] == null ){ // 3.만약에 index번째 요소가 비어있으면
+                boards[index] = board; // 배열에 새로운 객체 저장
+                return true; // 함수종료 뜻, true 반환 했으므로 리턴타입은 boolean
+            }
+        } // for end
+        return false; // for문 끝났음에도 저장 못했으면 false 반환
+    } // method end
+    // 기능2 : 조회 메소드
+    Board[ ] doGet( ){
+        return boards; // boards 반환 했으므로 리턴타입은 boards 변수의 타입은 Board[ ] 배열이다.
+    }
+} // class end
+
---
+++ src/종합/예제4/BoardService.java
@@ -0,0 +1,53 @@
+package 종합.예제4;
+
+import java.util.Scanner;
+
+// - 실행클래스 : 입출력 담당
+public class BoardService {
+    // -- 최초 실행 함수 = main
+    public static void main(String[] args) {
+        for( ; ; ){ // [1] 반복문
+            System.out.println("============ My Community ============ ");
+            System.out.println("1.게시물쓰기 2.게시물출력");
+            System.out.println("====================================== ");
+            System.out.print("선택>");Scanner scan = new Scanner( System.in );
+            int ch = scan.nextInt();
+            if( ch == 1 ){
+                scan.nextLine();
+                System.out.print("내용 : ");  String content = scan.nextLine();
+                System.out.print("작성자 : "); String writer = scan.nextLine();
+                // BoardController 의 저장 기능 호출 하여 저장 결과 받기
+                BoardController bc = new BoardController();
+                boolean result = bc.doPost( content , writer );
+                if( result == true ){ System.out.println("[안내] 저장 성공");}
+                else{ System.out.println("[경고] 저장 실패 "); }
+
+            }else if( ch == 2 ){
+                // BoardController 의 조회 기능 호출 하여 조회 결과 받기
+                BoardController bc = new BoardController();
+                Board[ ] boards = bc.doGet();
+                for( int index = 0 ; index<= boards.length-1 ; index++ ){
+                    if( boards[index] != null ){
+                        System.out.println( "작성자 : "+boards[index].writer );
+                        System.out.println( "내용 : "+boards[index].content);
+                        System.out.println("--------------------------------");
+                    }
+                }
+            }
+        }
+    }
+}// class end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
