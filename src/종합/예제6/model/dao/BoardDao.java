06caf940e614ec374dbcf84ec5dbac5594b6fd92
itdanja2025 <itdanja2025@gmail.com>
2026-01-29 16:03:03 +0900

    day08

--- src/종합/예제6/model/dao/BoardDao.java
+++ src/종합/예제6/model/dao/BoardDao.java
@@ -1,9 +1,24 @@
 package 종합.예제6.model.dao;
 
+import 종합.예제6.model.dto.BoardDto;
+import java.util.ArrayList;
+
 public class BoardDao {
     private BoardDao(){}
     private static  final BoardDao instance = new BoardDao();
     public static BoardDao getInstance(){
         return instance;
     }
+    // [*] 데이터베이스 역할 하는 ArrayList < 추후에 삭제 예정 >
+    private ArrayList<BoardDto> boardDB = new ArrayList<>();
 }
+
+
+
+
+
+
+
+
+
+
